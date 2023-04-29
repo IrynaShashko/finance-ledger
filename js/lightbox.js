3420d61566bc3b3e87bf54a0f7ad2ae76012d9d8
IrynaShashko <irishkashashko@gmail.com>
2023-04-29 16:40:38 +0300

    fix

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,78 +1,11 @@
-// імпортуємо бібліотеку lightbox2
-import 'lightbox2';
+import SimpleLightbox from 'simplelightbox';
 
-// налаштування lightbox2
-lightbox.option({
-  resizeDuration: 200,
-  wrapAround: true,
-  alwaysShowNavOnTouchDevices: true,
-  fadeDuration: 500,
-  disableScrolling: true,
-  showImageNumberLabel: false,
-  wrapAround: true,
-  positionFromTop: 50,
-  disableScrolling: true,
-  albumLabel: 'Зображення %1 з %2',
-  disableScrolling: true,
-  showImageNumberLabel: false,
-  disableScrolling: true,
-  fadeDuration: 500,
-  alwaysShowNavOnTouchDevices: true,
-  disableScrolling: true,
+// const galery = document.querySelector('#galery div a');
+
+const gallery = new SimpleLightbox('#galery div a', {
+  caption: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  nav: true,
+  navText: ['&lsaquo', 'rsaquo'],
 });
-
-// відстеження подій на кнопках переключення та закриття
-document.addEventListener('click', function (event) {
-  // перевірка, щоб перевірити, чи натиснуто на кнопку "назад"
-  if (event.target.classList.contains('lb-prev')) {
-    // переключення на попереднє зображення
-    lightbox.prev();
-  }
-
-  // перевірка, щоб перевірити, чи натиснуто на кнопку "вперед"
-  if (event.target.classList.contains('lb-next')) {
-    // переключення на наступне зображення
-    lightbox.next();
-  }
-
-  // перевірка, щоб перевірити, чи натиснуто на кнопку "закрити"
-  if (event.target.classList.contains('lb-close')) {
-    // закриття вікна зображення
-    lightbox.close();
-  }
-});
-
-// // Отримати всі елементи з класом "lightbox"
-// const lightboxElements = document.querySelectorAll('.lightbox');
-// console.log('lightboxElements', lightboxElements);
-// // Додати подію "click" до кожного зображення
-// lightboxElements.forEach(img => {
-//   console.log('img', img);
-//   img.addEventListener('click', function () {
-//     console.log('yahoooo');
-//     // Створити модальне вікно та зображення
-//     const lightbox = document.createElement('div');
-//     const lightboxImg = document.createElement('img');
-//     const closeButton = document.createElement('span');
-//     console.log('lightbox', lightbox);
-//     // Додати id для модального вікна та кнопки закриття
-//     lightbox.id = 'lightbox';
-//     closeButton.id = 'close';
-
-//     console.log('lightbox.id ', lightbox.id);
-
-//     // Додати зображення в модальне вікно та додати модальне вікно до body
-//     lightboxImg.src = this.src;
-//     lightbox.appendChild(lightboxImg);
-//     document.body.appendChild(lightbox);
-
-//     // Додати кнопку закриття до модального вікна
-//     closeButton.innerHTML = '&times;';
-//     lightbox.appendChild(closeButton);
-
-//     // Додати подію "click" до кнопки закриття
-//     closeButton.addEventListener('click', function () {
-//       lightbox.remove();
-//     });
-//   });
-// });
