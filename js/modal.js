7a02ffad70fa91a3b7a18a50cf92dd94c32fc65f
IrynaShashko <irishkashashko@gmail.com>
2023-05-12 19:42:04 +0300

    fix

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 const modal = document.querySelector('.modal-btn');
 
 modal.addEventListener('click', function (event) {
-  window.location.href = '/';
+  // window.location.href = '/';
   if (event.target.tagName === 'BUTTON') {
     modal.classList.remove('show-modal');
     modal.classList.add('hidden');
