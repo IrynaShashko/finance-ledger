91e36fd48ea6dce20a4739d88767a577ade551c6
IrynaShashko <irishkashashko@gmail.com>
2023-05-12 12:32:53 +0300

    fix

--- js/form.js
+++ js/form.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('#my-form');
-const submitButton = form.querySelector('button[type="submit"]');
+// const submitButton = form.querySelector('button[type="submit"]');
 const input = form.querySelector('input[required]');
 
-const emailInput = form.querySelector('#email');
+// const emailInput = form.querySelector('#email');
 const errorContainer = emailInput.nextElementSibling;
 
 // form.addEventListener('submit', function (event) {
@@ -18,10 +18,6 @@ const errorContainer = emailInput.nextElementSibling;
 
 //   //   form.reset();
 // });
-
-if (email === '') {
-  console.log(email);
-}
 
 input.addEventListener('input', function (event) {
   if (event.target.value.trim() === '') {
