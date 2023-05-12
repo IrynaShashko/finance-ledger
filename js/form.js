6031d78e07801719ee7a95f57dbc85da90f89c04
IrynaShashko <irishkashashko@gmail.com>
2023-05-12 13:33:35 +0300

    fix

--- js/form.js
+++ js/form.js
@@ -2,30 +2,26 @@ const form = document.querySelector('#my-form');
 const input = form.querySelector('input[required]');
 const errorContainer = input.nextElementSibling;
 
-// Add input event listener to show/hide error message
-input.addEventListener('input', function (event) {
+// Add function to show/hide error message
+function ShowError(event) {
   if (event.target.value === '') {
     errorContainer.classList.remove('show');
   } else {
     errorContainer.classList.add('show');
   }
-});
+}
+
+// Add input event listener to show/hide error message
+input.addEventListener('input', ShowError);
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const nameInput = form.querySelector('#name');
   const emailInput = form.querySelector('#email');
   const emailValue = emailInput.value;
   const emailRegex = /\S+@\S+\.\S+/; // Regular expression to match email format
 
   // Remove input event listener
-  input.removeEventListener('input', function (event) {
-    if (event.target.value === '') {
-      errorContainer.classList.remove('show');
-    } else {
-      errorContainer.classList.add('show');
-    }
-  });
+  input.removeEventListener('input', ShowError);
 
   if (emailValue.match(emailRegex)) {
     // Email format is valid, submit the form
--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal');
 
 modal.addEventListener('click', function (event) {
+  window.location.href = '/index.html';
   if (event.target.tagName === 'BUTTON') {
     modal.classList.remove('show-modal');
     modal.classList.add('hidden');
