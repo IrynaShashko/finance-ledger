1008e8e84ba78905856910b5bea1803f68ed9898
IrynaShashko <irishkashashko@gmail.com>
2023-05-16 22:10:27 +0300

    fix

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('.header');
+const headerHeight = window.innerWidth < 768 ? 120 : 70;
+
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 120) {
+  if (window.scrollY >= headerHeight) {
     header.classList.add('is-scrolling');
   } else {
     header.classList.remove('is-scrolling');
